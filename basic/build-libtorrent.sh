2ba2bd5d477c9ae1aa51b3a9a243a17e87b2399e
userdocs <16525024+userdocs@users.noreply.github.com>
2021-11-14 12:43:09 +0000

    Update build-libtorrent.sh

--- basic/build-libtorrent.sh
+++ basic/build-libtorrent.sh
@@ -6,7 +6,7 @@
 #
 # docker run -it -w /root -e "LANG=en_GB.UTF-8" -v $HOME/build:/root ubuntu:bionic /bin/bash -c 'apt update && apt install -y curl && curl -sL git.io/JXDOJ | bash -s boost_v=76 build_d=yes libtorrent_b= cxxstd=14 libtorrent= python_b= python_v= lto=on'
 #
-# ./build-libtorrent.sh boost_v= build_d= libtorrent_b= cxxstd= libtorrent= python_b= python_v= lto=
+# ./build-libtorrent.sh boost_v= build_d= libtorrent_b= cxxstd= libtorrent= python_b= python_v= lto= crypto=
 #
 #  If these are unset they default
 #
@@ -27,14 +27,17 @@ done
 [[ "$(source /etc/os-release && printf '%s' "$VERSION_CODENAME")" =~ (stretch|bionic) && "${python_v}" == 'python2' ]] && python_v="python"
 #
 ## Defaults are set here
-boost_v="${boost_v:-77}"                    # set the boost version using just 74/75/76/77
-build_d="$(pwd)/${build_d:-lt-build}"       # set the build directory - default is 3 lt-build relative to the container /root
-install_d="${build_d}-completed"            # set the completed directory based of the build dir name
-libtorrent_b="${libtorrent_b:-RC_2_0}"      # set the libtorrent branch to use - default is RC_2_0
-cxxstd="${cxxstd:-17}"                      # set the cxx standard 11/14/17 - default is 17
-libtorrent=${libtorrent:-yes}               # built libtorrent yes/no - default is yes
-python_b="${python_b:-yes}"                 # build the python binding yes/no - default is yes
-python_v="${python_v:-python_v}"            # set the python version 2/3 - default is 3
+boost_v="${boost_v:-77}"               # set the boost version using just 74/75/76/77
+build_d="$(pwd)/${build_d:-lt-build}"  # set the build directory - default is 3 lt-build relative to the container /root
+install_d="${build_d}-completed"       # set the completed directory based of the build dir name
+libtorrent_b="${libtorrent_b:-RC_2_0}" # set the libtorrent branch to use - default is RC_2_0
+cxxstd="${cxxstd:-17}"                 # set the cxx standard 11/14/17 - default is 17
+libtorrent=${libtorrent:-yes}          # built libtorrent yes/no - default is yes
+python_b="${python_b:-yes}"            # build the python binding yes/no - default is yes
+python_v="${python_v:-python_v}"       # set the python version 2/3 - default is 3
+crypto="${crypto:-openssl}"            # set wolfssl as alternative to opensll (default)
+CXXFLAGS="-std=c++${cxxstd:-17} -fPIC" # Set some basic CXXFLAGS
+#
 [[ -n "${lto}" ]] && lto="lto=on" || lto="" # set values for boost the build dir and the liborrent branch - default is null . On or null are the options
 #
 printf '\n%s\n\n' "${green} Update env and install core deps${end}"
@@ -43,9 +46,22 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y build-essential curl pkg-config git perl "${python_v}" "${python_v}-dev" zlib1g-dev libssl-dev # install the deps
 #
-printf '\n%s\n\n' "${green} Values being used ${cyan}:${yellow} Boost version = 1.${boost_v}.0 ${cyan}:${yellow} build dir = ${build_d} ${cyan}:${yellow} libtorrent branch = ${libtorrent_b} ${cyan}:${yellow} CXX standard = ${cxxstd} ${cyan}:${yellow} libtorrent=${libtorrent} ${cyan}:${yellow} python_b=${python_b} ${cyan}:${yellow} python_v=$("${python_v}" -c "import sys; print(sys.version_info[0])") ${cyan}:${yellow} ${lto:-lto=off}${end}"
+printf '\n%s\n\n' "${green} Values being used ${cyan}:${yellow} Boost version = 1.${boost_v}.0 ${cyan}:${yellow} build dir = ${build_d} ${cyan}:${yellow} libtorrent branch = ${libtorrent_b} ${cyan}:${yellow} CXX standard = ${cxxstd} ${cyan}:${yellow} libtorrent=${libtorrent} ${cyan}:${yellow} python_b=${python_b} ${cyan}:${yellow} python_v=$("${python_v}" -c "import sys; print(sys.version_info[0])") ${cyan}:${yellow} ${lto:-lto=off} ${cyan}:${yellow} crypto=${crypto} ${end}"
 #
-printf '%s\n\n' "${green} Download and bootstrap ${magenta}boost_1_${boost_v}_0${end}"
+if [[ "${crypto}" == 'wolfssl' ]]; then
+	printf '%s\n\n' "${green} Download and bootstrap ${magenta}wolfssl${end}"
+	wolfssl_github_tag="$(grep -Eom1 'v([0-9.]+?)-stable$' <(curl -sL "https://github.com/wolfSSL/wolfssl/tags"))"
+	git clone --no-tags --single-branch --branch "${wolfssl_github_tag}" --shallow-submodules --recurse-submodules --depth 1 "https://github.com/wolfSSL/wolfssl.git" "${build_d}/wolfssl"
+	cd "${build_d}/wolfssl" || exit
+	./autogen.sh
+	./configure --enable-static --disable-shared --enable-asio --enable-sni --enable-nginx CXXFLAGS="$CXXFLAGS"
+	make -j"$(nproc)"
+	crypto=("crypto=wolfssl" "wolfssl-lib=${build_d}/wolfssl/src/.libs" "wolfssl-include=${build_d}/wolfssl")
+	printf '\n%s\n\n' "${green} Download and bootstrap ${magenta}boost_1_${boost_v}_0${end}"
+else
+	crypto=("crypto=openssl")
+	printf '%s\n\n' "${green} Download and bootstrap ${magenta}boost_1_${boost_v}_0${end}"
+fi
 #
 if [[ ! -f "${build_d}/boost_1_${boost_v}_0/b2" ]]; then
 	curl -sNLk "https://boostorg.jfrog.io/artifactory/main/release/1.${boost_v}.0/source/boost_1_${boost_v}_0.tar.gz" --create-dirs -o "${build_d}/boost_1_${boost_v}_0.tar.gz"
@@ -74,7 +90,7 @@ cd "${build_d}/libtorrent" || exit
 if [[ "${libtorrent:-no}" == yes ]]; then
 	printf '\n%s\n\n' "${green} Build libtorrent ${libtorrent_b}${end}"
 	#
-	"${build_d}/boost_1_${boost_v}_0/b2" -j"$(nproc)" address-model="$(getconf LONG_BIT)" "${lto}" optimization=speed cxxstd="${cxxstd}" variant=release dht=on encryption=on crypto=openssl i2p=on extensions=on threading=multi link=static boost-link=static install --prefix="${install_d}"
+	"${build_d}/boost_1_${boost_v}_0/b2" -j"$(nproc)" address-model="$(getconf LONG_BIT)" "${lto}" optimization=speed cxxstd="${cxxstd}" variant=release dht=on encryption=on "${crypto[@]}" i2p=on extensions=on threading=multi link=static boost-link=static install --prefix="${install_d}"
 	#
 	printf '\n%s\n\n' "${green} Files are located at: ${cyan}${install_d}/lib${end}"
 else
@@ -93,7 +109,7 @@ if [[ "${python_b}" == yes ]]; then
 	#
 	printf '%s\n\n' "${green} Build libtorrent ${libtorrent_b} pything bindings${end}"
 	#
-	"${build_d}/boost_1_${boost_v}_0/b2" -j"$(nproc)" address-model="$(getconf LONG_BIT)" fpic=on "${lto}" optimization=speed cxxstd="${cxxstd}" variant=release dht=on encryption=on crypto=openssl i2p=on extensions=on threading=multi libtorrent-link=static boost-link=static install_module python-install-scope=user
+	"${build_d}/boost_1_${boost_v}_0/b2" -j"$(nproc)" address-model="$(getconf LONG_BIT)" fpic=on "${lto}" optimization=speed cxxstd="${cxxstd}" variant=release dht=on encryption=on "${crypto[@]}" i2p=on extensions=on threading=multi libtorrent-link=static boost-link=static install_module python-install-scope=user
 	#
 	printf '\n%s\n\n' "${green} Python binding file is located at: ${cyan}$("${python_v}" -c "import site; import sys; sys.stdout.write(site.USER_SITE)")/libtorrent.so${end}"
 	printf '%s\n\n' "${green} Python binding version is: ${cyan}$("${python_v}" -c "import libtorrent; print(libtorrent.version)")${end}"
